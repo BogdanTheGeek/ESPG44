d3994944b7bc0143eabdf158c867d1737a3817ed
moonking33 <47536171+moonking3@users.noreply.github.com>
2019-02-12 22:26:38 +0000

    Create demo1_main.cpp
    yue added the main file for demo1

---
+++ demo1_main.cpp
@@ -0,0 +1,132 @@
+//
+//  demo1_main.cpp
+//  
+//
+//  Created by yue on 12/02/2019.
+//
+
+#include "mbed.h"
+#include "motors.h"
+
+
+
+int main (void)
+{
+    //create object
+    Motor* m_Motor = new Motor() ;
+    
+    //wait for 6 seconds to prepare
+    wait (6.0);
+    
+    //start tracking the square routine
+    
+    //set 'travel distance' and 'wheel speed (Right and Left)' ,
+    //distance in mm, speed scale 0-1
+    
+    //*****  clockwise  *****
+    //step 1    O==O ---------> 500mm,  ^stop!^
+    m_Motor->move_distance_R(500, 0.3);
+    m_Motor->move_distance_L(500, 0.3);
+    
+    //step 2    O==O ------>     //turning right for 90 degrees, with a speed of 0.3 (scale 0-1)
+    //                      |
+    //                      |
+    //                      |
+    //                      |
+    //                      v
+    //                      O
+    //                      ||
+    //                      O
+    m_Motor->turn(90.0, 0.3);
+    
+    //step 3    O==O ---------> 500mm,  ^stop!^
+    m_Motor->move_distance_R(500, 0.3);
+    m_Motor->move_distance_L(500, 0.3);
+    
+    //step 4    O==O ------>     //turning right for 90 degrees, with a speed of 0.3 (scale 0-1)
+    //                      |
+    //                      |
+    //                      |
+    //                      |
+    //                      v
+    //                      O
+    //                      ||
+    //                      O
+    m_Motor->turn(90.0, 0.3);
+    
+    //step 5    O==O ---------> 500mm,  ^stop!^
+    m_Motor->move_distance_R(500, 0.3);
+    m_Motor->move_distance_L(500, 0.3);
+    
+    //step 6    O==O ------>     //turning right for 90 degrees, with a speed of 0.3 (scale 0-1)
+    //                      |
+    //                      |
+    //                      |
+    //                      |
+    //                      v
+    //                      O
+    //                      ||
+    //                      O
+    m_Motor->turn(90.0, 0.3);
+    
+    //step 7    O==O ---------> 500mm,  ^stop!^  ,  now the buggy is back to the origin
+    m_Motor->move_distance_R(500, 0.3);
+    m_Motor->move_distance_L(500, 0.3);
+    
+    //******   anti-clockwise   ******
+    //step 1   turning around at the origin
+    m_Motor->turn(180.0, 0.3);
+    
+    //step 2   O==O ---------> 500mm,  ^stop!^
+    m_Motor->move_distance_R(500, 0.3);
+    m_Motor->move_distance_L(500, 0.3);
+    
+    //step 3    O==O <------^    //turning left for 90 degrees, with a speed of 0.3 (scale 0-1)
+    //                      |
+    //                      |
+    //                      |
+    //                      |
+    //
+    //                      O
+    //                      ||
+    //                      O
+    m_Motor->turn(-90.0, 0.3);
+    
+    //step 4   O==O ---------> 500mm,  ^stop!^
+    m_Motor->move_distance_R(500, 0.3);
+    m_Motor->move_distance_L(500, 0.3);
+    
+    //step 5    O==O <------^    //turning left for 90 degrees, with a speed of 0.3 (scale 0-1)
+    //                      |
+    //                      |
+    //                      |
+    //                      |
+    //
+    //                      O
+    //                      ||
+    //                      O
+    m_Motor->turn(-90.0, 0.3);
+    
+    //step 6   O==O ---------> 500mm,  ^stop!^
+    m_Motor->move_distance_R(500, 0.3);
+    m_Motor->move_distance_L(500, 0.3);
+    
+    //step 7    O==O <------^    //turning left for 90 degrees, with a speed of 0.3 (scale 0-1)
+    //                      |
+    //                      |
+    //                      |
+    //                      |
+    //
+    //                      O
+    //                      ||
+    //                      O
+    m_Motor->turn(-90.0, 0.3);
+    
+    //step 6   O==O ---------> 500mm,  ^stop!^
+    m_Motor->move_distance_R(500, 0.3);
+    m_Motor->move_distance_L(500, 0.3);
+    
+    
+    
+  
+}
