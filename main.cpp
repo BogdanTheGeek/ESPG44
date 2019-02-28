fea0034a45f650bf2739aeb621766b5e19585889
moonking33 <47536171+moonking3@users.noreply.github.com>
2019-02-28 14:41:38 +0000

    added pid support and new main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,69 @@
+#include "pid.h"
 #include "motors.h"
+#include "mbed.h"
+#include "sensors.h"
 
-int main() {
+int main(void)
+{
+	Motor *pp_motors = new Motor();
+	ScanLine *pp_Scanline = new ScanLine();
+	pid *pp_pid = new pid(parameters); //void pid::pid (double dt, double max, double min, double kp, double kd, double ki)
 
+	switch (pp_pid->WORKING_STATE)
+	{
+		//*****Stop mode
+	case pp_pid->Stop:
+		pp_motors->set_speed_R(0);
+		pp_motors->set_speed_L(0);
+		//if bluetooth signal = 1, change to Follow mode --- else stay in Stop mode
+		if ()
+		{
+			pp_pid->WORKING_STATE = pp_pid->Follow;
+		}
+		else
+		{
+		}
 
-	
+		//******Follow mode
+	case pp_pid->Follow:
+		// apply pid control
+		pp_pid->keep_calculate_PID(); //  repeat calculating pid and apply into pwm
+
+		//if bluetooth signal = 1, change to Stop mode
+		if ()
+		{
+			pp_pid->WORKING_STATE = pp_pid->Stop;
+		}
+		//when bluetooth signal = 0, check if sensors on the line
+		// #1 no output via front sensors , also , front middle sensor no output--> wait for seconds to check the back sensor
+		else if (pp_Scanline->array_to_value_V2() == 0 && pp_ScanLine->value[3] == 0)
+		{
+			wait(0.5); // wait 0.5s, if (front mid sensor & back sensor) no signal, stop wheels, and change to turning mode
+			if (pp_Scanline->value[0] == 0 && pp_ScanLine->value[3] == 0)
+			{
+				pp_motors->set_speed_R(0);
+				pp_motors->set_speed_L(0);
+
+				pp_pid->WORKING_STATE = pp_pid->Turning;
+			}
+			//#2   else stay in Follow mode
+			else
+			{
+				pp_pid->WORKING_STATE = pp_pid->Follow;
+			}
+		}
+
+		//******Turning mode
+	case pp_pid->Turning:
+		pp_motors->turn(180, 0.2);
+
+		pp_pid->WORKING_STATE = pp_pid->Follow;
+
+		//check bluetooth signal to stop or not
+
+		if ()
+		{
+			pp_pid->WORKING_STATE = pp_pid->Stop;
+		}
+	}
 }
---
+++ pid.h
@@ -0,0 +1,42 @@
+#include "mbed.h"
+#include "motors.h"
+#include "sensors.h"
+#define base_speed_r  0.2
+#define base_speed_l  0.2
+//pid basic variables
+#define DT 0.1
+#define MAX 0.8
+#define MIN -1.2
+#define KP 10
+#define KD 10
+#define KI 10
+
+class pid {
+private:
+
+  
+//class pointers
+  ScanLine *p_ScanLine;
+  Motor  *p_Motor  ;
+//ticker objects
+  Ticker Ticker_pid;
+  
+public:
+      
+  enum working_state 
+      {
+        Follow,
+        Stop,
+        Turning
+      }
+  enum working_state WORKING_STATE = Stop;
+
+  //constructor
+  void pid (void);
+
+  //pid output 
+  void pid_calculation(void);
+  void keep_calculate_PID(void);
+  
+  
+};
