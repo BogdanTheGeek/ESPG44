066da1c917c6a152f8adf1155d4a31dea19865b6
BogdanTheGeek <bobby.b.tiger@gmail.com>
2019-03-09 23:47:46 +0000

    integrated Ahmad's code for bluetooth with a few modifications to adapt it for the demo. Now bloetooth switches between the different types of serial output.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include "sensors.h"
 
 #define DELAY 0.2
+#define ON_key "esp44"
+#define OFF_key "44esp"
+
 
 int state = 0;
+
+void bluetooth_handler(void);
+Serial hm10(BLETX, BLERX, 9600);
+
 
 int main() {
 
 	ScanLine *sensors = new ScanLine();
 	Serial serial(USBTX, USBRX, 115200);
+	hm10.attach(&bluetooth_handler, Serial::RxIrq);
 
 	while(true){
 		sensors->scan();
@@ -28,4 +36,24 @@ int main() {
 		wait(DELAY);
 	}
 
+}
+
+void bluetooth_handler(void){
+	char buffer[6];
+
+	int i = 0;
+	while(i < 5){
+		buffer[i++] = hm10.getc();
+	}
+
+	if(strcmp(buffer, ON_key) == 0){ 
+	    state = 1;
+	   	return;
+	}
+	if(strcmp(buffer, OFF_key) == 0){ 
+		state = 0;
+		return;
+	}
+
+	return;
 }
